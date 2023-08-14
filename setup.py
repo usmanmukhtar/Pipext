bcbdabf0f94f09f0ebdcd91fe999eb6aced8e85f
Usman Mukhtar <usmanmukhtar432@gmail.com>
2023-08-14 06:22:30 -0700

    updated setup.py for showing readme doc in pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='pipext',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
     ],
@@ -13,4 +17,6 @@ setup(
     },
     author='Usman Mukhtar',
     description='An extended pip installer that updates requirements.txt',
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
 )
